5ce6a3eb566c752fbc6e190d0f059cb6dcaacfd4
kazuya.kawakita <61093699+takiwaka@users.noreply.github.com>
2021-10-19 06:54:27 +0900

    三項演算子

--- src/index.js
+++ src/index.js
@@ -79,11 +79,30 @@ const nameArr = ["kawakita", "yamada", "sato"];
 
 // console.log(newNumArr);
 
-const newNameArr = nameArr.map((name) => {
-  if (name === "kawakita") {
-    return name;
-  } else {
-    return `${name}さん`;
-  }
-});
-console.log(newNameArr);
+// const newNameArr = nameArr.map((name) => {
+//   if (name === "kawakita") {
+//     return name;
+//   } else {
+//     return `${name}さん`;
+//   }
+// });
+// console.log(newNameArr);
+
+// 三項演算子
+//使いすぎると可読性下がるので注意
+//条件？条件がtrueの時:falseの時
+// const val = 1 > 0 ? "trueです" : "farseです";
+// console.log(val);
+
+// const num = "1300";
+// console.log(num.toLocaleString());
+
+// const formattedNum =
+//   typeof num === "number" ? num.toLocaleString() : "数値を入力してください";
+// console.log(formattedNum);
+
+// const checkSum = (num1, num2) => {
+//   return num1 + num2 > 100 ? "100を超えています" : "100以下です";
+// };
+
+// console.log(checkSum(50, 51));
