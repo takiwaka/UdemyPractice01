a5abce03debdf967e0a217aaf7250537a4d645be
kazuya.kawakita <61093699+takiwaka@users.noreply.github.com>
2021-10-18 15:31:43 +0900

    宣言の勉強

--- src/index.js
+++ src/index.js
@@ -4,8 +4,27 @@ document.getElementById("app").innerHTML = `
 <h1>Hello Vanilla!</h1>
 `;
 
-console.log("test");
-
 var test1 = "varは再宣言、再代入可能";
 
 console.log(test1);
+
+let test2 = "letは再代入不可、再宣言可能";
+
+console.log(test2);
+
+// できない→let test2 ="再代入できなかった、再宣言は出来た。"
+test2 = "再代入できなかった、再宣言は出来た。";
+
+console.log(test2);
+
+const test3 =
+  "constは再代入も再宣言もできないが、オブジェクトのプロパティは変更可能";
+
+console.log(test3);
+
+const test4 = { name: "kawakita", age: 32 };
+
+test4.name = "kazuya";
+test4.address = "Tokyo";
+
+console.log(test4);
