33690c03c95d35edcaa233836cfde6eb19d56d08
kazuya.kawakita <61093699+takiwaka@users.noreply.github.com>
2021-10-18 15:42:27 +0900

    アロー関数の勉強

--- src/index.js
+++ src/index.js
@@ -4,27 +4,33 @@ document.getElementById("app").innerHTML = `
 <h1>Hello Vanilla!</h1>
 `;
 
-var test1 = "varは再宣言、再代入可能";
+// var test1 = "varは再宣言、再代入可能";
 
-console.log(test1);
+// console.log(test1);
 
-let test2 = "letは再代入不可、再宣言可能";
+// let test2 = "letは再代入不可、再宣言可能";
 
-console.log(test2);
+// console.log(test2);
 
-// できない→let test2 ="再代入できなかった、再宣言は出来た。"
-test2 = "再代入できなかった、再宣言は出来た。";
+// // できない→let test2 ="再代入できなかった、再宣言は出来た。"
+// test2 = "再代入できなかった、再宣言は出来た。";
 
-console.log(test2);
+// console.log(test2);
 
-const test3 =
-  "constは再代入も再宣言もできないが、オブジェクトのプロパティは変更可能";
+// const test3 =
+//   "constは再代入も再宣言もできないが、オブジェクトのプロパティは変更可能";
 
-console.log(test3);
+// console.log(test3);
 
-const test4 = { name: "kawakita", age: 32 };
+// const test4 = { name: "kawakita", age: 32 };
 
-test4.name = "kazuya";
-test4.address = "Tokyo";
+// test4.name = "kazuya";
+// test4.address = "Tokyo";
 
-console.log(test4);
+// console.log(test4);
+
+// アロー関数
+
+const arrow = () => console.log("アロー関数");
+
+arrow();
